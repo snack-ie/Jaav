9d400a03ec5ad730fd8c542eafc78eab8d6f4130
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-09-03 12:51:45 -0300

    Update and rename Mod.java to JaavMod.java

---
+++ src/jaav/JaavMod.java
@@ -0,0 +1,35 @@
+package jaav;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class JaavMod extends Mod{
+
+    public ExampleJavaMod(){
+
+        //listen for game load event
+        Events.on(ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("frog");
+                dialog.cont.add("behold, frog.").row();
+                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+                dialog.cont.image(Core.atlas.find("jaav-frog")).pad(20f).row();
+                dialog.cont.button("I love it.", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("FROGGE ATTACKKK");
+    }
+
+}
