c74ca1cb2700104c8a06b22652c474aad6408d62
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-09-03 14:01:48 -0300

    i dont plan on slowing down
    no ill keep on going
    even if it means im in the **wrong**

--- src/jaav/JaavMod.java
+++ src/jaav/JaavMod.java
@@ -16,14 +16,14 @@ public class JaavMod extends Mod{
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
-            Time.runTask(10f, () -> {
+
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold, frog.").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("jaav-frog")).pad(20f).row();
                 dialog.cont.button("I love it.", dialog::hide).size(100f, 50f);
                 dialog.show();
-            });
+
         });
     }
 
