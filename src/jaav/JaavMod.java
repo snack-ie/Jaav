ba84265faf5b88ffed41bd99bdd3f51e4d422d85
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-09-04 01:15:35 -0300

    add support for content

--- src/jaav/JaavMod.java
+++ src/jaav/JaavMod.java
@@ -8,26 +8,21 @@ import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
+import jaav.content.*;
 
 public class JaavMod extends Mod{
 
+    private final ContentList[] content = {
+        // new JaavUnits()
+    };
+
     public JaavMod(){
 
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            BaseDialog dialog = new BaseDialog("frog");
-            dialog.cont.add("behold, frog.").row();
-            //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-            dialog.cont.image(Core.atlas.find("jaav-frog")).pad(20f).row();
-            dialog.cont.button("I love it.", dialog::hide).size(100f, 50f);
-            dialog.show();
-        });
     }
-
     @Override
     public void loadContent(){
-        Log.info("FROGGE ATTACKKK");
+        for(ContentList list : content){
+            list.load();
+        }
     }
-
 }
