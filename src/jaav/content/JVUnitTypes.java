93f24df78f3cb98625ef1ea290219de6b6b43e6b
Pietro303HD <pietro303hd@gmail.com>
2021-09-04 11:55:03 -0300

    I CAN Float NOW

--- src/jaav/content/JVUnitTypes.java
+++ src/jaav/content/JVUnitTypes.java
@@ -74,7 +74,7 @@ public class JVUnitTypes implements ContentList{
 
         EntityMapping.nameMap.put("bust", SplitUnitEntity::new);
         bust = new SplitUnitType("bust"){
-           health = 100f;
+           Float health = 100f;
            String test = "hello!";
         };
     };
